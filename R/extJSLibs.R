1eee2ffdffc7c6ebe97b80896e581f25a2573eec
Tim Appelhans <tim.appelhans@addium.io>
2026-03-05 15:37:11 +0100

    add details for parquet-wasm to extJSLibs()

--- R/extJSLibs.R
+++ R/extJSLibs.R
@@ -4,19 +4,20 @@
 #' `geoarrowWidget`.
 #'
 #' See e.g. \url{https://cdn.jsdelivr.net/npm/apache-arrow/package.json}
-#' or \url{https://cdn.jsdelivr.net/npm/@geoarrow/geoarrow-js/package.json} for
-#' more details on the js depencencies.
+#' or \url{https://cdn.jsdelivr.net/npm/@geoarrow/geoarrow-js/package.json} or
+#' \url{https://cdn.jsdelivr.net/npm/parquet-wasm/package.json} for
+#' more details on the JavaScript depencencies.
 #'
 #' @returns
-#'   A named character vector with the versions of the `GeoArrow` and `Arrow`
-#'   JavaScript libraries shipped with this package.
+#'   A named character vector with the versions of the `GeoArrow`, `Arrow`
+#'   and `Parquet-WASM` JavaScript libraries shipped with this package.
 #'
 #' @examples
 #'   extJSLibs()
 #'
 #' @tests tinytest
-#' expect_length(extJSLibs(), 2)
-#' expect_length(names(extJSLibs()), 2)
+#' expect_length(extJSLibs(), 3)
+#' expect_length(names(extJSLibs()), 3)
 #'
 #' @export
 extJSLibs = function() {
@@ -25,10 +26,12 @@ extJSLibs = function() {
     c(
       .geoarrowJSDependency()[[1]]$version
       , .arrowJSDependency()[[1]]$version
+      , .parquetWasmDependencies()[[1]]$version
     )
     , names = c(
       .geoarrowJSDependency()[[1]]$name
       , .arrowJSDependency()[[1]]$name
+      , .parquetWasmDependencies()[[1]]$name
     )
   )
 
